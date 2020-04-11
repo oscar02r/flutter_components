658e6a8eb5eb747af85af2e6e134dd9821ae5da0
oscar02r <oscary.ramirez@gmail.com>
2020-04-11 16:31:20 +0200

    Added component home page temp

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:component_flutter/pages/home_page_temp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp  extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+           debugShowCheckedModeBanner: false,
+           home: Scaffold(
+           appBar: AppBar(
+            title: Text('Hola Mundo'),
+          ),
+          body: HomePageTemp(),
+        ),
+      );
+  }
+}
