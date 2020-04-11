ee32c953d32fad90230ffe2cc15819e20012075e
oscar02r <oscary.ramirez@gmail.com>
2020-04-11 16:44:34 +0200

    Added first list view to temp home

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,7 @@ class MyApp  extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
            debugShowCheckedModeBanner: false,
-           home: Scaffold(
-           appBar: AppBar(
-            title: Text('Hola Mundo'),
-          ),
-          body: HomePageTemp(),
-        ),
+           home: HomePageTemp()
       );
   }
 }
--- lib/pages/home_page_temp.dart
+++ lib/pages/home_page_temp.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-         child: Text('Home Page Temp'),
+    return Scaffold(
+        appBar: AppBar(title: Text('Home Temp'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title:Text("List Title list"),
+            ),
+            Divider(),
+            ListTile(
+              title:Text("List Title list"),
+            ),
+               Divider(),
+            ListTile(
+              title:Text("List Title list"),
+            ),
+          ],
+        ),
     );
   }
 }
