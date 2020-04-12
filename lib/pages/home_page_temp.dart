1dfb7b4762c5eef4650cb7ffae92359ced35cdee
oscar02r <oscary.ramirez@gmail.com>
2020-04-12 03:59:44 +0200

    Way long use list

--- lib/pages/home_page_temp.dart
+++ lib/pages/home_page_temp.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final options = [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+    'Option 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Home Temp'),
+        appBar: AppBar(
+          title: Text('Home Temp')
         ),
         body: ListView(
-          children: <Widget>[
-            ListTile(
-              title:Text("List Title list"),
-            ),
-            Divider(),
-            ListTile(
-              title:Text("List Title list"),
-            ),
-               Divider(),
-            ListTile(
-              title:Text("List Title list"),
-            ),
-          ],
-        ),
-    );
+          children: _createItems())
+        );
+  }
+
+  List<Widget> _createItems() {
+    final List<Widget> list = new List<Widget>();
+
+    for (String opt in options) {
+        
+        final tempWidget = ListTile(
+          title: Text(opt)
+        );
+
+      list..add(tempWidget )
+      ..add(Divider(color:Colors.deepPurpleAccent));
+    }
+    return list;
   }
 }
